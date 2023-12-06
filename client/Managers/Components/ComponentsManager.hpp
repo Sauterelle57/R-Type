14fa413685e82657da5c8082fa46ba00d5fd8a7c
Sauterelle57 <marie.giacomel@epitech.eu>
2023-12-06 10:56:05 +0100

    add (feat/ECS): file architecture for managers

---
+++ client/Managers/Components/ComponentsManager.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** RType
+** File description:
+** ComponentsManager
+*/
+
+#ifndef COMPONENTSMANAGER_HPP_
+    #define COMPONENTSMANAGER_HPP_
+
+class ComponentsManager {
+    public:
+        ComponentsManager();
+        ~ComponentsManager();
+
+    protected:
+    private:
+};
+
+#endif /* !COMPONENTSMANAGER_HPP_ */
---
+++ client/Managers/Entities/EntitiesManager.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** RType
+** File description:
+** EntitiesManager
+*/
+
+#ifndef ENTITIESMANAGER_HPP_
+    #define ENTITIESMANAGER_HPP_
+
+class EntitiesManager {
+    public:
+        EntitiesManager();
+        ~EntitiesManager();
+
+    protected:
+    private:
+};
+
+#endif /* !ENTITIESMANAGER_HPP_ */
