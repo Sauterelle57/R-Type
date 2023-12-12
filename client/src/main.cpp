5952d562ca70a5075535ced3519a2f5ed7c4e0a0
epi-noahg <noah.gosciniak@epitech.eu>
2023-12-12 14:31:52 +0100

    add (feat/shaders): lighting shaders

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,87 +1,86 @@
-/*
-** EPITECH PROJECT, 2023
-** RType
-** File description:
-** ComponentsManager
-*/
 
 #include "raylib.h"
 #include "renderer/Window.hpp"
 #include "renderer/ICamera.hpp"
 #include "renderer/Camera.hpp"
 #include "renderer/renderer.hpp"
-#include <math.h>
+
+#include "raymath.h"
+
+#define RLIGHTS_IMPLEMENTATION
+#include "rlights.h"
 #include <memory>
 
-int main()
+int main(void)
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    std::shared_ptr<ZappyGui::IWindow> window = std::make_shared<ZappyGui::ZWindow>(screenWidth, screenHeight, "R TYPE WINDOW");
+    ZappyGui::Utils::setConfigFlags(FLAG_MSAA_4X_HINT);
+    std::shared_ptr<ZappyGui::IWindow> window = std::make_shared<ZappyGui::ZWindow>(screenWidth, screenHeight, "R TYPE test light");
 
     std::shared_ptr<ZappyGui::ICamera> camera = std::make_shared<ZappyGui::ZCamera>();
-    camera->setPosition((Vector3){ 30.0f, 20.0f, 30.0f });
-    camera->setTarget((Vector3){ 0.0f, 0.0f, 0.0f });      // Camera looking at point
-    camera->setUp((Vector3){ 0.0f, 1.0f, 0.0f });          // Camera up vector (rotation towards target)
-    camera->setFovy(70.0f);                                // Camera field-of-view Y
-    camera->setProjection(CAMERA_PERSPECTIVE);             // Camera projection type
+    camera->setPosition((Vector3){ 2.0f, 4.0f, 6.0f });
+    camera->setTarget((Vector3){ 0.0f, 0.0f, 0.0f });
+    camera->setUp((Vector3){ 0.0f, 1.0f, 0.0f });
+    camera->setFovy(45.0f);
+    camera->setProjection(CAMERA_PERSPECTIVE);
 
-    // Specify the amount of blocks in each direction
-    const int numBlocks = 15;
+    std::shared_ptr<ZappyGui::ZMesh> planMesh = std::make_shared<ZappyGui::ZMesh>();
+    planMesh->genPlane(10.0f, 10.0f, 3, 3);
+    planMesh->setCanUnload(false);
+    std::shared_ptr<ZappyGui::ZMesh> cubeMesh = std::make_shared<ZappyGui::ZMesh>();
+    cubeMesh->genCube(2.0f, 4.0f, 2.0f);
+    cubeMesh->setCanUnload(false);
+    std::shared_ptr<ZappyGui::ZModel> planModel = std::make_shared<ZappyGui::ZModel>(planMesh->getMesh());
+    std::shared_ptr<ZappyGui::ZModel> cubeModel = std::make_shared<ZappyGui::ZModel>(cubeMesh->getMesh());
+    std::shared_ptr<ZappyGui::ZShader> shader = std::make_shared<ZappyGui::ZShader>("./client/resources/shaders/lighting.vs", "./client/resources/shaders/lighting.fs");
 
-    ZappyGui::Utils::setTargetFPS(60);
-    //--------------------------------------------------------------------------------------
+    shader->getShader()->locs[SHADER_LOC_VECTOR_VIEW] = shader->getLocation("viewPos");
 
-    // Main game loop
-    while (!window->shouldClose())
+    int ambientLoc = shader->getLocation("ambient");
+    float test[4] = { 0.1f, 0.1f, 0.1f, 1.0f };
+    shader->setValue(ambientLoc, test, SHADER_UNIFORM_VEC4);
+
+    planModel->getModel()->materials[0].shader = *shader->getShader();
+    cubeModel->getModel()->materials[0].shader = *shader->getShader();
+
+    Light lights[MAX_LIGHTS] = { 0 };
+    lights[0] = CreateLight(LIGHT_POINT, (Vector3){ -2, 1, -2 }, Vector3Zero(), YELLOW, *shader->getShader());
+    lights[1] = CreateLight(LIGHT_POINT, (Vector3){ 2, 1, 2 }, Vector3Zero(), RED, *shader->getShader());
+    lights[2] = CreateLight(LIGHT_POINT, (Vector3){ -2, 1, 2 }, Vector3Zero(), GREEN, *shader->getShader());
+    lights[3] = CreateLight(LIGHT_POINT, (Vector3){ 2, 1, -2 }, Vector3Zero(), BLUE, *shader->getShader());
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        double time = ZappyGui::Utils::getTime();
+        camera->update(CAMERA_ORBITAL);
 
-        float scale = (2.0f + (float)sin(time))*0.7f;
+        float cameraPos[3] = { camera->getPosition().x, camera->getPosition().y, camera->getPosition().z };
+        SetShaderValue(*shader->getShader(), shader->getShader()->locs[SHADER_LOC_VECTOR_VIEW], cameraPos, SHADER_UNIFORM_VEC3);
 
-        // Move camera around the scene
-        double cameraTime = time*0.3;
-        camera->setPosition({(float) cos(cameraTime) * 40.0f, (float) sin(cameraTime) * 40.0f, camera->getPosition().z});
+        if (IsKeyPressed(KEY_Y)) { lights[0].enabled = !lights[0].enabled; }
+        if (IsKeyPressed(KEY_R)) { lights[1].enabled = !lights[1].enabled; }
+        if (IsKeyPressed(KEY_G)) { lights[2].enabled = !lights[2].enabled; }
+        if (IsKeyPressed(KEY_B)) { lights[3].enabled = !lights[3].enabled; }
+
+        for (int i = 0; i < MAX_LIGHTS; i++) UpdateLightValues(*shader->getShader(), lights[i]);
         window->beginDrawing();
         window->clearBackground(RAYWHITE);
-
         camera->beginMode();
+        planModel->draw(Vector3Zero(), 1.0f, WHITE);
+        cubeModel->draw(Vector3Zero(), 1.0f, WHITE);
 
-        window->drawGrid(10, 5.0f);
-
-        for (int x = 0; x < numBlocks; x++)
-        {
-            for (int y = 0; y < numBlocks; y++)
-            {
-                for (int z = 0; z < numBlocks; z++)
-                {
-                    // Scale of the blocks depends on x/y/z positions
-                    float blockScale = (x + y + z)/30.0f;
-
-                    // Scatter makes the waving effect by adding blockScale over time
-                    float scatter = sinf(blockScale*20.0f + (float)(time*4.0f));
-
-                    // Calculate the cube position
-                    Vector3 cubePos = {
-                            (float)(x - numBlocks/2)*(scale*3.0f) + scatter,
-                            (float)(y - numBlocks/2)*(scale*2.0f) + scatter,
-                            (float)(z - numBlocks/2)*(scale*3.0f) + scatter
-                    };
-
-                    // Pick a color with a hue depending on cube position for the rainbow color effect
-                    Color cubeColor = ZappyGui::Utils::colorFromHSV((float)(((x + y + z)*18)%360), 0.75f, 0.9f);
-
-                    // Calculate cube size
-                    float cubeSize = (2.4f - scale)*blockScale;
-                    window->drawCube(cubePos, cubeSize, cubeSize, cubeSize, cubeColor);
-                }
-            }
+        for (int i = 0; i < MAX_LIGHTS; i++) {
+            if (lights[i].enabled) DrawSphereEx(lights[i].position, 0.2f, 8, 8, lights[i].color);
+            else DrawSphereWires(lights[i].position, 0.2f, 8, 8, ColorAlpha(lights[i].color, 0.3f));
         }
+
+        window->drawGrid(10, 1.0f);
         camera->endMode();
         window->drawFPS(10, 10);
+        window->drawText("Use keys [Y][R][G][B] to toggle lights", 10, 40, 20, DARKGRAY);
         window->endDrawing();
     }
     return 0;
