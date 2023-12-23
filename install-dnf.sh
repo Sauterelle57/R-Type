b0597023bad2f810fdfd1eb0fc2922d5a7cbe8f9
CPU64x <q.erdinger@orange.fr>
2023-12-24 00:30:06 +0100

    fix (feat/install-script): install dependencies for SFML part

--- install-dnf.sh
+++ install-dnf.sh
@@ -7,6 +7,6 @@ echo "Installing GIT..."
 sudo dnf install git -y
 
 echo "Installing SFML Packages..."
-sudo dnf install libXi-devel freetype-devel libX11-devel libXrandr-devel libudev-devel mesa-libGL-devel flac-devel libogg-devel libvorbis-devel libvorbisenc2 libvorbisfile3 openal-soft-devel libpthread-stubs-devel libjpeg-turbo-devel libsndfile-devel libXcursor-devel libXinerama-devel -y
+sudo dnf install libXi-devel freetype-devel libX11-devel libXrandr-devel libX11-devel lesa-libGL-devel systeld-devel openal-soft-devel libvorbis-devel flac-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel libudev-devel mesa-libGL-devel flac-devel libogg-devel libvorbis-devel libvorbisenc2 libvorbisfile3 openal-soft-devel libpthread-stubs-devel libjpeg-turbo-devel libsndfile-devel libXcursor-devel libXinerama-devel freetype-devel -y
 
 echo "SFML Packages Installed!"
