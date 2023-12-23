4fd64cf60caf637da6a1a76c56596dcc6b7142b6
CPU64x <quentin.erdinger@epitech.eu>
2023-12-23 18:17:19 +0100

    fix (feat/install-script): fix script for apt / windows

--- install.bat
+++ install.bat
@@ -3,5 +3,5 @@ cmake -DCMAKE_PREFIX_PATH="C:/Users/lucas/vcpkg/packages/raylib_x64-windows" ..
 
 
 # on linux : 
-sudo apt-get update
-sudo apt-get install libpthread-stubs0-dev libgl1-mesa-dev libx11-dev libxrandr-dev libfreetype6-dev libjpeg8-dev libsndfile1-dev libopenal-dev libudev-dev libxcb-image0-dev libjpeg-dev libflac-dev libvorbis-dev libogg-dev libxcursor-dev
+sudo apt-get update -y
+sudo apt-get install libpthread-stubs0-dev libgl1-mesa-dev libx11-dev libxrandr-dev libfreetype6-dev libjpeg8-dev libsndfile1-dev libopenal-dev libudev-dev libxcb-image0-dev libjpeg-dev libflac-dev libvorbis-dev libogg-dev libxcursor-dev -y
