a89cf6fa2d1a0bd8d46771f43680e24a35c605d7
CPU64x <quentin.erdinger@epitech.eu>
2023-12-30 10:59:12 +0100

    fix (feat/serialization): fix protocol controller

--- protocol/Protocol.hpp
+++ protocol/Protocol.hpp
@@ -30,20 +30,38 @@ namespace rt
         ENTITIES
     };
 
+    enum ENTITY_TYPE
+    {
+        PLAYER,
+        ENEMY,
+    };
+
     struct Entity
     {
         std::uint32_t ECSEntity;
         tls::Vec3 position;
         tls::Vec4 rotation;
         float scale;
-        std::string type;
+        ENTITY_TYPE entityType;
+    };
+
+    // Client
+    struct p_client {
+        tls::Vec3 move = {0, 0, 0};
+    };
+
+    // Server
+    struct p_server {
+        std::vector<Entity> entities; // List entities from server to client
     };
 
     struct Protocol
     {
         SENDER_TYPE sender;
         PROTOCOL_TYPE protocol;
-        std::vector<Entity> entities;
+
+        p_client client;
+        p_server server;
     };
 
     class ProtocolController
@@ -70,17 +88,42 @@ namespace rt
             return *this;
         }
 
-        ProtocolController& addEntity(std::uint32_t ECSId, tls::Vec3 position, tls::Vec4 rotation, float scale, std::string type)
+        // Actions :
+
+        ProtocolController& actionPing()
         {
+            _protocol->sender = CLIENT;
+            _protocol->protocol = rt::PROTOCOL_TYPE::PING;
+            return *this;
+        }
+
+        ProtocolController& actionMove(tls::Vec3 move)
+        {
+            _protocol->sender = rt::SENDER_TYPE::CLIENT;
+            _protocol->protocol = rt::PROTOCOL_TYPE::MOVE;
+
+            _protocol->client.move = move;
+            return *this;
+        }
+
+        // Events Server Part :
+
+        ProtocolController& addEntity(std::uint32_t ECSId, tls::Vec3 position, tls::Vec4 rotation, float scale, ENTITY_TYPE type)
+        {
+            _protocol->sender = rt::SENDER_TYPE::SERVER;
+            _protocol->protocol = rt::PROTOCOL_TYPE::ENTITIES;
+
             Entity entity;
             entity.ECSEntity = ECSId;
             entity.position = position;
             entity.rotation = rotation;
             entity.scale = scale;
-            entity.type = type;
-            _protocol->entities.push_back(entity);
+            entity.entityType = type;
+            _protocol->server.entities.push_back(entity);
             return *this;
         }
+
+        // Functions : 
 
         std::string serialize() const
         {
@@ -88,7 +131,7 @@ namespace rt
             oss.write(reinterpret_cast<const char*>(&_protocol->sender), sizeof(_protocol->sender));
             oss.write(reinterpret_cast<const char*>(&_protocol->protocol), sizeof(_protocol->protocol));
 
-            for (const auto& entity : _protocol->entities)
+            for (const auto& entity : _protocol->server.entities)
             {
                 serializeEntity(oss, entity);
             }
@@ -102,7 +145,7 @@ namespace rt
             oss.write(reinterpret_cast<const char*>(&protocol.sender), sizeof(protocol.sender));
             oss.write(reinterpret_cast<const char*>(&protocol.protocol), sizeof(protocol.protocol));
 
-            for (const auto& entity : protocol.entities)
+            for (const auto& entity : protocol.server.entities)
             {
                 serializeEntity(oss, entity);
             }
@@ -122,7 +165,7 @@ namespace rt
             {
                 Entity entity;
                 deserializeEntity(iss, entity);
-                deserializedData.entities.push_back(entity);
+                deserializedData.server.entities.push_back(entity);
             }
 
             return deserializedData;
@@ -139,10 +182,7 @@ namespace rt
             oss.write(reinterpret_cast<const char*>(&entity.position), sizeof(entity.position));
             oss.write(reinterpret_cast<const char*>(&entity.rotation), sizeof(entity.rotation));
             oss.write(reinterpret_cast<const char*>(&entity.scale), sizeof(entity.scale));
-
-            std::size_t typeSize = entity.type.size();
-            oss.write(reinterpret_cast<const char*>(&typeSize), sizeof(typeSize));
-            oss.write(entity.type.c_str(), typeSize);
+            oss.write(reinterpret_cast<const char*>(&entity.entityType), sizeof(entity.entityType));
         }
 
         static void deserializeEntity(std::istringstream& iss, Entity& entity)
@@ -151,11 +191,7 @@ namespace rt
             iss.read(reinterpret_cast<char*>(&entity.position), sizeof(entity.position));
             iss.read(reinterpret_cast<char*>(&entity.rotation), sizeof(entity.rotation));
             iss.read(reinterpret_cast<char*>(&entity.scale), sizeof(entity.scale));
-
-            std::size_t typeSize;
-            iss.read(reinterpret_cast<char*>(&typeSize), sizeof(typeSize));
-            entity.type.resize(typeSize);
-            iss.read(&entity.type[0], typeSize);
+            iss.read(reinterpret_cast<char*>(&entity.entityType), sizeof(entity.ECSEntity));
         }
     };
 
--- server/main.cpp
+++ server/main.cpp
@@ -12,6 +12,7 @@
 
 #include "ServerController.hpp"
 #include "GameController.hpp"
+#include "Protocol.hpp"
 
 void signalHandler(int signum) {
     if (signum == SIGINT) {
@@ -21,6 +22,35 @@ void signalHandler(int signum) {
 }
 
 int main() {
+    rt::ProtocolController pc;
+    pc.init();
+
+    pc.setSender(rt::SENDER_TYPE::SERVER);
+    pc.setProtocol(rt::PROTOCOL_TYPE::ENTITIES);
+
+    pc.addEntity(1, {1, 2, 3}, {0, 0, 0, 90}, 0.25f, rt::ENTITY_TYPE::PLAYER);
+    pc.addEntity(2, {10, 11, 12}, {90, 0, 90, 180}, 1, rt::ENTITY_TYPE::ENEMY);
+    pc.addEntity(3, {13, 14, 15}, {1, 1, 0, 180}, 2, rt::ENTITY_TYPE::ENEMY);
+
+    auto result = pc.serialize();
+
+    auto uncompressed = rt::ProtocolController::deserialize(result);
+
+    std::cout << uncompressed.sender << std::endl;
+    std::cout << uncompressed.protocol << std::endl;
+    for (auto &x : uncompressed.server.entities) {
+        std::cout << "entity : ";
+        std::cout << x.ECSEntity << std::endl;
+        std::cout << "pos :" << x.position._x << ", " << x.position._y << ", " << x.position._z << std::endl;
+        std::cout << "rotation: " << x.rotation._x << ", " << x.rotation._y << ", " << x.rotation._z << ", " << x.rotation._a << std::endl;
+        std::cout << "scale: "  << x.scale << std::endl;
+        std::cout << "type: " << x.entityType << std::endl;
+        std::cout << "---------------" << std::endl;
+    }
+    
+
+
+    return 0;
     std::signal(SIGINT, signalHandler);
     std::shared_ptr<rt::IGameController> gameCtrl = std::make_shared<rt::GameController>();
     std::shared_ptr<rt::IServerController> serverCtrl = std::make_shared<rt::ServerController>(1234, gameCtrl);
