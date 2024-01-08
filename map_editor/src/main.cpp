67ad063fbf0bd70525ec97d2213cc479ddbd180e
axide47 <91666656+axide47@users.noreply.github.com>
2024-01-08 18:59:57 +0100

    add (feat/map_editor): gui structure

--- map_editor/src/main.cpp
+++ map_editor/src/main.cpp
@@ -1,76 +1,90 @@
-
 #include "Renderer.hpp"
-
 #define RAYGUI_IMPLEMENTATION
-#include "raygui.h"                 // Required for GUI controls
+#define MAX_GESTURE_STRINGS   20
+#include "raygui.h"
+#include <iostream>
+#include <string>
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
+int main()
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1500;
+    const int screenHeight = 800;
+    int segments = 0;
+    std::shared_ptr<RL::IWindow> window = std::make_shared<RL::ZWindow>(screenWidth, screenHeight, "map editor");
 
-    std::shared_ptr<RL::IWindow> window = std::make_shared<RL::ZWindow>(screenWidth, screenHeight, "maman");
+    Vector2 touchPosition = { 0, 0 };
+    Rectangle touchArea = { 1225, 0, screenWidth - 500, screenHeight};
+    int gesturesCount = 0;
+    char gestureStrings[MAX_GESTURE_STRINGS][32];
+    int currentGesture = GESTURE_NONE;
+    int lastGesture = GESTURE_NONE;
 
-    float roundness = 0.2f;
-    float width = 200.0f;
-    float height = 100.0f;
-    float segments = 0.0f;
-    float lineThick = 1.0f;
+    RL::Utils::setTargetFPS(60);
+    while (!window->shouldClose()) {
 
-    bool drawRect = false;
-    bool drawRoundedRect = true;
-    bool drawRoundedLines = false;
+        lastGesture = currentGesture;
+        currentGesture = GetGestureDetected();
+        touchPosition = GetTouchPosition(0);
 
-    RL::Utils::setTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+        if (CheckCollisionPointRec(touchPosition, touchArea) && (currentGesture != GESTURE_NONE)) {
+            if (currentGesture != lastGesture) {
+                switch (currentGesture) {
+                    case GESTURE_TAP: TextCopy(gestureStrings[gesturesCount], "GESTURE TAP"); break;
+                    case GESTURE_HOLD: TextCopy(gestureStrings[gesturesCount], "GESTURE HOLD"); break;
+                    case GESTURE_DRAG: TextCopy(gestureStrings[gesturesCount], "GESTURE DRAG"); break;
+                    default: break;
+                }
+                gesturesCount++;
 
-    // Main game loop
-    while (!window->shouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        Rectangle rec = { ((float)GetScreenWidth() - width - 250)/2, (GetScreenHeight() - height)/2.0f, (float)width, (float)height };
-        //----------------------------------------------------------------------------------
+                // Reset gestures strings
+                if (gesturesCount >= MAX_GESTURE_STRINGS) {
+                    for (auto & gestureString : gestureStrings) TextCopy(gestureString, "\0");
+                    gesturesCount = 0;
+                }
+            }
+        }
 
-        // Draw
-        //----------------------------------------------------------------------------------
+
+
+
+
+
         window->beginDrawing();
 
-        window->clearBackground(RAYWHITE);
+        window->clearBackground(LIGHTGRAY);
+        window->drawLine(1200, 0, 1200, GetScreenHeight(), Fade(BLACK, 0.6f));
+        window->drawRectangle(1200, 0, GetScreenWidth() - 500, GetScreenHeight(), Fade(WHITE, 0.3f)); // zone d'entité à droite
+        window->drawRectangle(50, 50, GetScreenWidth() - 400, GetScreenHeight() - 250, Fade(WHITE, 0.3f)); // content windows
 
-        window->drawLine(560, 0, 560, window->getScreenHeight(), RL::Utils::fade(LIGHTGRAY, 0.6f));
-        window->drawRectangle(560, 0, window->getScreenWidth() - 500, window->getScreenHeight(), RL::Utils::fade(LIGHTGRAY, 0.3f));
+        Rectangle scrollBarRect = { 50, 625, 1100, 20 };
+        GuiSetStyle(SCROLLBAR, SCROLL_SLIDER_SIZE, 64);
+        segments = GuiScrollBar(scrollBarRect, segments, 0, 100);
+        window->drawRectangle(scrollBarRect.x - 5, scrollBarRect.y - 5, scrollBarRect.width + 10, scrollBarRect.height + 10, Fade(BLACK, 0.3f));
 
-        if (drawRect) window->drawRectangleRec(rec, RL::Utils::fade(GOLD, 0.6f));
-        if (drawRoundedRect) window->drawRectangleRounded(rec, roundness, (int)segments, RL::Utils::fade(MAROON, 0.2f));
-        if (drawRoundedLines) window->drawRectangleRoundedLines(rec, roundness, (int)segments, lineThick, RL::Utils::fade(MAROON, 0.4f));
 
-        // Draw GUI controls
-        //------------------------------------------------------------------------------
-        GuiSliderBar({ 640, 40, 105, 20 }, "Width", NULL, &width, 0, static_cast<float>(window->getScreenWidth() - 300));
-        GuiSliderBar({ 640, 70, 105, 20 }, "Height", NULL, &height, 0, static_cast<float>(window->getScreenHeight() - 50));
-        GuiSliderBar({ 640, 140, 105, 20 }, "Roundness", NULL, &roundness, 0.0f, 1.0f);
-        GuiSliderBar({ 640, 170, 105, 20 }, "Thickness", NULL, &lineThick, 0, 20);
-        GuiSliderBar({ 640, 240, 105, 20}, "Segments", NULL, &segments, 0, 60);
 
-        GuiCheckBox({ 640, 320, 20, 20 }, "DrawRoundedRect", &drawRoundedRect);
-        GuiCheckBox({ 640, 350, 20, 20 }, "DrawRoundedLines", &drawRoundedLines);
-        GuiCheckBox({ 640, 380, 20, 20}, "DrawRect", &drawRect);
-        //------------------------------------------------------------------------------
 
-        std::string mode = std::string("MODE: ") + ((segments >= 4)? "MANUAL" : "AUTO");
+        window->drawText("current gestures: ", 500, 250, 30, GRAY);
+        window->drawText(gestureStrings[gesturesCount - 1], 500, 300, 30, GRAY);
+        window->drawText("touche position:", 500, 370, 30, GRAY);
+        window->drawText(std::to_string(static_cast<int>(touchPosition.x)), 500, 400, 30, GRAY);
+        window->drawText(std::to_string(static_cast<int>(touchPosition.y)), 500, 430, 30, GRAY);
 
-        window->drawText(mode, 640, 280, 10, (segments >= 4)? MAROON : DARKGRAY);
 
+        if (currentGesture != GESTURE_NONE) { // Draw gestures
+            DrawCircleV(touchPosition, 30, MAROON);
+        }
+
+
+
+
+
+
+        std::cout << "scrollbar: " << segments << std::endl;
         window->drawFPS(10, 10);
-
         window->endDrawing();
-        //----------------------------------------------------------------------------------
     }
+    CloseWindow();
+
     return 0;
 }
