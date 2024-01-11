835ad9521f9ae6b612b6a03f4a6c873973cff6a9
epi-noahg <noah.gosciniak@epitech.eu>
2024-01-12 00:17:48 +0100

    fix (dev): max entities, 200000 -> 2000 (bad pc compilation)

--- server/Tools/ECS/Managers/Entities/Entity.hpp
+++ server/Tools/ECS/Managers/Entities/Entity.hpp
@@ -15,7 +15,8 @@
 namespace ECS {
     using Entity = std::uint32_t;
 
-    constexpr Entity MAX_ENTITIES = 2000;
+    constexpr Entity MAX_ENTITIES = 200
+            0;
 
     using Signature = std::bitset<MAX_COMPONENTS>;
 }
