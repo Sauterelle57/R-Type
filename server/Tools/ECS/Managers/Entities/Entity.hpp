5d18d2c027fea11a92fdbc75a2aa488fb835c458
CPU64x <quentin.erdinger@epitech.eu>
2024-01-12 09:29:23 +0100

    fix: (dev): max Entities to 2000 entities

--- server/Tools/ECS/Managers/Entities/Entity.hpp
+++ server/Tools/ECS/Managers/Entities/Entity.hpp
@@ -15,8 +15,7 @@
 namespace ECS {
     using Entity = std::uint32_t;
 
-    constexpr Entity MAX_ENTITIES = 200
-            0;
+    constexpr Entity MAX_ENTITIES = 2000;
 
     using Signature = std::bitset<MAX_COMPONENTS>;
 }
