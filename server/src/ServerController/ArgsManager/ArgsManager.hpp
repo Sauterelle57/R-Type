22cdb7a838ab727dceade4297b1a5146e62fd4c8
CPU64x <quentin.erdinger@epitech.eu>
2024-01-14 16:47:42 +0100

    add (feat/flag-manager): -p to set a custom server running port

---
+++ server/src/ServerController/ArgsManager/ArgsManager.hpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2024
+** ArgsManager
+** File description:
+** ArgsManager
+*/
+
+#ifndef ARGSMANAGER_HPP_
+#define ARGSMANAGER_HPP_
+#include "IArgsManager.hpp"
+
+namespace rt {
+
+    class ArgsManager : public IArgsManager {
+        public:
+            ArgsManager() = default;
+            ~ArgsManager() = default;
+
+            std::optional<int> getServerPort(int argc, char **argv) const {
+                for (int i = 1; i < argc; i++) {
+                    if (std::string(argv[i]) == "-p") {
+                        if (i + 1 < argc) {
+                            int port = std::atoi(argv[i + 1]);
+                            return port;
+                        } else {
+                            return std::nullopt;
+                        }
+                    }
+                }
+                return std::nullopt;
+            }
+    };
+
+}
+
+#endif /* !ARGSMANAGER_HPP_ */
---
+++ server/src/ServerController/ArgsManager/IArgsManager.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPP-500-STG-5-2-rtype-noah.gosciniak
+** File description:
+** IArgsManager
+*/
+
+#ifndef IARGSMANAGER_HPP_
+#define IARGSMANAGER_HPP_
+#include <iostream>
+#include <cstdlib>
+#include <optional>
+
+namespace rt {
+
+    class IArgsManager {
+        public:
+            virtual ~IArgsManager() = default;
+            virtual std::optional<int> getServerPort(int argc, char* argv[]) const = 0;
+    };
+
+}
+
+#endif /* !IARGSMANAGER_HPP_ */
