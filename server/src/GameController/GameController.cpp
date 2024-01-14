98f5424a9f26086e97557d3303a40c0b27f1ae1a
CPU64x <quentin.erdinger@epitech.eu>
2024-01-14 23:29:03 +0100

    add (feat/file-input): add map file path with -m [MAP]

--- server/src/GameController/GameController.cpp
+++ server/src/GameController/GameController.cpp
@@ -10,10 +10,10 @@
 
 namespace rt {
 
-    GameController::GameController(bool debug)
+    GameController::GameController(bool debug, std::string mapFilePath)
     {
         try {
-            const std::string path = "map_editor/map_test.json";
+            const std::string path = mapFilePath;
             _data = lvl::jsonParsing(path);
         } catch (const std::exception &e) {
             std::cerr << e.what() << std::endl;
