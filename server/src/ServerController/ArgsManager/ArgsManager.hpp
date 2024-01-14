d1fba839088352e4461463e77f9f71d650f91bb0
CPU64x <quentin.erdinger@epitech.eu>
2024-01-14 17:25:07 +0100

    fix (feat/flag-manager): check if input is a number

--- server/src/ServerController/ArgsManager/ArgsManager.hpp
+++ server/src/ServerController/ArgsManager/ArgsManager.hpp
@@ -8,6 +8,7 @@
 #ifndef ARGSMANAGER_HPP_
 #define ARGSMANAGER_HPP_
 #include "IArgsManager.hpp"
+#include <cctype>
 
 namespace rt {
 
@@ -20,6 +21,10 @@ namespace rt {
                 for (int i = 1; i < argc; i++) {
                     if (std::string(argv[i]) == "-p") {
                         if (i + 1 < argc) {
+                            for (int x = 0; argv[i + 1][x]; x++) {
+                                if (!std::isdigit(argv[i + 1][x]))
+                                    return std::make_optional<int>(-1);
+                            }
                             int port = std::atoi(argv[i + 1]);
                             return port;
                         } else {
